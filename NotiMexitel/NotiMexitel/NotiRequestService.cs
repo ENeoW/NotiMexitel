c8ce95fa2551dfd63cf3b68c41a2aee83e56bf48
PRICETRAVEL\eloy.pedroso <eloy.pedroso@pricetravel.com>
2018-05-07 11:36:15 -0500

    change from local storage to static variable.

--- NotiMexitel/NotiMexitel/NotiRequestService.cs
+++ NotiMexitel/NotiMexitel/NotiRequestService.cs
@@ -8,32 +8,39 @@ namespace NotiMexitel
    {
       const string Mexico = "https://embamex.sre.gob.mx/cuba/index.php/avisos/159-info-de-sc-citas-mexitel";
       const string Panama = "http://200.46.78.75/portal_migracion_digital/views/visa_cuba.php";
-      
+
+      private static string oldPageMexico = string.Empty;
+      private static string oldPagePanama = string.Empty;
+
       public bool GetMexitelNotification()
       {
-         return GetNotification(Mexico, nameof(Mexico));
+         return GetNotification(Mexico, nameof(Mexico), ref oldPageMexico);
       }
-      
+
       public bool GetPanamaNotification()
       {
-         return GetNotification(Panama, nameof(Panama));
+         return GetNotification(Panama, nameof(Panama), ref oldPagePanama);
       }
 
 
-      private bool GetNotification(string url, string target)
+      private bool GetNotification(string url, string target, ref string oldPage)
       {
          bool result = false;
          var currentPage = string.Empty;
-         var oldPage = string.Empty;
-         if(Application.Current.Properties.ContainsKey(target))
-            oldPage = (string)Application.Current.Properties[target];
-         using (var httpClient = new HttpClient())
-            currentPage = httpClient.GetStringAsync(Mexico).Result;
+         try
+         {
+            using (var httpClient = new HttpClient())
+               currentPage = httpClient.GetStringAsync(Mexico).Result;
+         }
+         catch (Exception e)
+         {
+            throw new Exception($"No se pudo acceder a la página de {target}, revise su conexión a internet");
+         }
          if (oldPage != currentPage)
          {
             if (string.IsNullOrWhiteSpace(oldPage))
             {
-               App.Current.Properties[target] = currentPage;
+               oldPage = currentPage;
                throw new Exception($"Se descargó la página de {target} para comparar");
             }
             result = true;
